a25337ce7beb0c02ea497c6dd470bf1dbbe3055b
portiaportia <portiaplante@gmail.com>
2024-05-17 12:23:18 -0400

    redeploying with hasrouting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Layout from "./pages/layout";
 import Home from "./pages/home";
 import About from "./pages/about";
@@ -8,7 +8,7 @@ import NoPage from "./pages/nopage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -17,7 +17,7 @@ export default function App() {
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
