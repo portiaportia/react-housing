39f0961c74e93c4c90e9140ca1e9dac98a22cee9
portiaportia <portiaplante@gmail.com>
2024-05-17 12:42:34 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import NoPage from "./pages/nopage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
