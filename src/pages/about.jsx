498ea125cc813867349d6294032727da7554c696
portiaportia <portiaplante@gmail.com>
2024-05-24 11:57:49 -0400

    added a slideshow

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
+import "../styles/slideshow.css";
+
 const About = () => {
-  return <h1>About Us</h1>;
+  function importAll(r) {
+    return r.keys().map(r);
+  }
+
+  const images = importAll(
+    require.context("../images/slideshow", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  const [featuredImageIndex, setFeaturedImageIndex] = useState(0);
+
+  const slideForward = () => {
+    setFeaturedImageIndex(
+      featuredImageIndex === images.length - 1 ? 0 : featuredImageIndex + 1
+    );
+  };
+
+  const slideBackward = () => {
+    setFeaturedImageIndex(
+      featuredImageIndex === 0 ? images.length - 1 : featuredImageIndex - 1
+    );
+  };
+
+  return (
+    <>
+      <h1>About Us</h1>
+      <div id="slideshow">
+        <img
+          key={images[featuredImageIndex]}
+          src={images[featuredImageIndex]}
+          alt={images[featuredImageIndex]}
+        />
+
+        <p id="forward-arrow" onClick={slideForward}>
+          &gt;
+        </p>
+        <p id="backward-arrow" onClick={slideBackward}>
+          &lt;
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default About;
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,5 +1,57 @@
+import { useState } from "react";
+import "../styles/contact.css";
+
 const Contact = () => {
-  return <h1>Contact Us</h1>;
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "2cc869cb-a5d4-4b4b-b609-7d0a0bd4ea78");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Email Successfully Sent");
+      event.target.reset(); //reset your form fields
+    } else {
+      console.log("Error sending email", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <div id="contact-form">
+      <form onSubmit={onSubmit}>
+        <p>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" id="name" required />
+        </p>
+
+        <p>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email" id="email" required />
+        </p>
+
+        <p>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" id="message" required></textarea>
+        </p>
+
+        <p>
+          <button type="submit">Submit Form</button>
+        </p>
+      </form>
+      <span>{result}</span>
+    </div>
+  );
 };
 
 export default Contact;
