93cc9ef73cb5cd9daef1fbce233092d524c070b6
portiaportia <portiaplante@gmail.com>
2024-05-24 10:42:38 -0400

    working toggle menu

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import Header from "../components/header";
 import Footer from "../components/footer";
@@ -5,16 +6,22 @@ import "../styles/nav.css";
 import "../styles/styles.css";
 
 const Layout = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div id="content">
       <Header />
       <nav id="main-nav">
-        <div id="toggle">
+        <div id="toggle" onClick={toggleMenu}>
           <div></div>
           <div></div>
           <div></div>
         </div>
-        <ul>
+        <ul id="nav-items" className={menuOpen ? "" : "hide-small"}>
           <li>
             <Link to="/">Home</Link>
           </li>
